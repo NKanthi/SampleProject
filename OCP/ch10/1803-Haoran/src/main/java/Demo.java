7c7bf605524bb77776a98b8fb2090efb85bd21f4
HaoranDing <hding@qualogy.com>
2018-01-11 22:19:45 +0100

    OCP 7 and 10

---
+++ OCP/ch10/1803-Haoran/src/main/java/Demo.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+public class Demo {
+
+    public static void main(String[] args) throws Exception{
+        Database database = Database.getInstance();
+        Map<Integer, Integer> ageMap = database.getAnimalAge();
+
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
+
+        Runnable run = () -> ageMap.entrySet().parallelStream().filter(entry -> entry.getValue()>2).forEach(entry -> System.out.print(entry.getValue()));
+        Callable call = () -> ageMap.entrySet().parallelStream().filter(entry -> entry.getValue()>2).map(entry -> entry.getKey()).collect(Collectors.toList());
+
+        service.schedule(run, 1, TimeUnit.SECONDS);
+        Future result = service.schedule(call, 5, TimeUnit.SECONDS);
+
+        System.out.println("The id of animal older than 2. "+ result.get());
+
+        service.shutdown();
+
+    }
+
+}
