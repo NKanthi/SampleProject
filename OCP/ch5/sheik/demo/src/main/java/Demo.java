e70cbf0aeb506477888ce18bc782e2d6cda445fe
ssoegriemsingh <ssoegriemsingh@qualogy.com>
2018-01-12 09:35:24 +0100

    Get default locale

--- OCP/ch5/sheik/demo/src/main/java/Demo.java
+++ OCP/ch5/sheik/demo/src/main/java/Demo.java
@@ -1,5 +1,10 @@
+import java.util.Locale;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.println("Hello world");
+
+        Locale locale = Locale.getDefault();
+        System.out.println(locale);
     }
 }
