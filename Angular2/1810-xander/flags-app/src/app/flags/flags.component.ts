f457ec0eb26522f06b87ce038e6ffdeced6f5472
XDIJK <xdijk@qualogy.com>
2018-03-02 12:51:49 +0100

    Initial commit

---
+++ Angular2/1810-xander/flags-app/src/app/flags/flags.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material'
+
+@Component({
+  selector: 'app-flags',
+  templateUrl: './flags.component.html',
+  styleUrls: ['./flags.component.css']
+})
+export class FlagsComponent implements OnInit {
+  countryA: object;
+  countryB: object;
+  ImgSrcCountryCode: string;
+  correctAnswer: string;
+  userAnswer: string;
+  countryCodesResponse: object;
+  outputMessage: string;
+
+  @Injectable()
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getCountriesFromJson()
+  }
+
+  getCountriesFromJson() {
+    this.http.get('../assets/countrycodes.json').subscribe(data => {
+      this.countryCodesResponse = data.countrycodes
+      this.setCountriesAndAwnser();
+    });
+  }
+
+  setCountriesAndAwnser() {
+      this.countryA = this.countryCodesResponse[this.getRandomCountry()];
+      this.countryB = this.countryCodesResponse[this.getRandomCountry()];
+
+    if(this.countryA == this.countryB) {
+      this.setCountriesAndAwnser();
+    }
+
+    if ( Math.round(Math.random()) ) {
+      this.correctAnswer = this.countryA.name;
+      this.ImgSrcCountryCode = this.countryA.code;
+    } else {
+      this.correctAnswer = this.countryB.name;
+      this.ImgSrcCountryCode = this.countryB.code;
+    }
+  }
+
+  getRandomCountry() {
+    return Math.floor(Math.random() * (this.countryCodesResponse.length));
+  }
+
+  UserSelectanswer(event: any) {
+    if(this.correctAnswer == event.target.textContent) {
+      this.outputMessage = "Correct"
+    } else {
+      this.outputMessage = "Wrong"
+    }
+  }
+
+  reset() {
+    this.setCountriesAndAwnser()
+    this.outputMessage = "";
+  }
+
+}
